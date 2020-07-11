1806dbf69e8df7f1918dcc8f636b1a8200746518
dataenggu <dsolkim1030@gmail.com>
2020-07-11 19:13:57 +0900

    Upload

--- Chapter 10 (Complier 1)/src/JackAnalyzer.java
+++ Chapter 10 (Complier 1)/src/JackAnalyzer.java
@@ -8,15 +8,19 @@ public class JackAnalyzer {
         /* file input */
 //        String inputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\SquareGame.jack";
 //        String inputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\Square.jack";
-//        String inputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\Main.jack";
+//        String inputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\Main.jack";/
+        /* 전체 디렉토리 분석일 떄 */
         String directoryPath = args[0];
+        System.out.println("directoryPath : " + directoryPath);
         File file;
         try {
             file = new File(directoryPath);
+            /* 디렉토리 내 java 파일 모두 읽음 */
             if (file.isDirectory()) {
                 String[] fileList = file.list();
                 for (String eachFile : fileList) {
                     if (eachFile.contains(".jack")) {
+                        System.out.println("sensed file : " + eachFile);
                         String inputFilePath = directoryPath + "\\" + eachFile;
                         String outputFilePath = directoryPath + "\\(JackTokenized)" + eachFile;
                         File inputFile = new File(inputFilePath);
@@ -25,17 +29,10 @@ public class JackAnalyzer {
                     }
                 }
             } else {
-
+                /* 지정 파일만 읽음*/
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-//        String outputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\SquareGameOUTPUT.xml";
-//        String outputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\SquareOUTPUT.xml";
-//        String outputFilePath = "P:\\The Elements of Computing Systems\\nand2tetris\\projects\\10\\ExpressionLessSquare\\MainOUTPUT.xml";
-//        File inputFile = new File(inputFilePath);
-//        File outputFile = new File(outputFilePath);
-//        CompilationEngine compilationEngine = new CompilationEngine(inputFile, outputFile);
     }
 }
